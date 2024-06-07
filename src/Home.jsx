e8ea4a50603e44e5b6aa5e6ac222e03cca52124c
Duvan Mancilla <Duvanred0@gmail.com>
2024-06-07 10:12:52 -0500

    casos de exito

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,17 @@
 import Hero from './componentes/Hero'
 import Mision from './componentes/Mision'
+import CasosExito from './componentes/CasosExito'
 export default function Home (){
     return(
         <>
         <Hero />
         <Mision />
+        <section className="casos">
+        <CasosExito caso="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus a provident rem asperiores sed fuga libero delectus quia quo itaque." />
+        <CasosExito caso="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus a provident rem asperiores sed fuga libero delectus quia quo itaque." />
+        <CasosExito caso="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus a provident rem asperiores sed fuga libero delectus quia quo itaque." />
+        <CasosExito caso="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus a provident rem asperiores sed fuga libero delectus quia quo itaque." /> 
+        </section>
         </>
     )
 
